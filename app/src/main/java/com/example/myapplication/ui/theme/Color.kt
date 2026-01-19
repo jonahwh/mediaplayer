a6a15a5b82dfedb6ef05ab76588bcdb0cc9377d9
Jonah Hirsch <jonahwh@gmail.com>
2026-01-19 15:44:19 -0800

    Update theming and typography

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Surface = Color(0xFF2e3240)
+
+val Selected = Color(0xFF004a77)
+
+val BarActive = Color(0xFF96989f)
+
+val BarInactive = Color(0xFF585b66)
